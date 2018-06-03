6bae7b5953c85ca22bfd30b861ce016d1a5bc29f
Radosław <radziowojak@gmail.com>
2018-06-03 14:36:26 +0200

    zrobione zadanie 6 z rozszerzonej matury z informatyki w starej formule z 2018 roku

---
+++ 2018 stara rozszerzona/6.1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+//matura rozszerzona 2018 w starej formule
+int main()
+{
+	string slowa[99999];
+	ifstream slowaP;
+	slowaP.open("slowa.txt");
+	if(slowaP.is_open())
+	{
+		int i=0;
+		while(slowaP>>slowa[i++]);
+		
+		//6.1
+		cout<<"6.1:\n";
+		int konczyA=0;
+		for(int j=0; j<i; j++)
+			if(slowa[j][ slowa[j].length()-1 ] == 'A')
+				konczyA++;
+		cout<<"Na 'A' konczy sie "<<konczyA<<" slow.\n";
+		
+		//6.2
+		cout<<"\n6.2:\n";
+		int zawiera=0;
+		for(int j=0; j<i-1; j+=2)
+			if(slowa[j+1].find(slowa[j]) != string::npos)
+			{
+				cout<<slowa[j]<<" zawiera sie w "<<slowa[j+1]<<endl;
+				zawiera++;
+			}
+		cout<<"Wierszy, w ktorych pierwsze slowo zawiera sie w drugim, jest "<<zawiera<<endl;
+		
+		//6.3
+		cout<<"\n6.3:\n";
+		int anagram=0;
+		for(int j=0; j<i-1; j+=2)
+			if(slowa[j].length() == slowa[j+1].length())
+			{
+				for(int k=0; k<slowa[j+1].length(); k++)
+				{
+					if(slowa[j+1].find(slowa[j][k]) != string::npos)
+					{
+						if(k == slowa[j+1].length()-1)
+						{
+							cout<<slowa[j]<<" jest anagramem "<<slowa[j+1]<<endl;
+							anagram++;
+						}
+					}
+					else
+						break;
+				}
+			}
+		cout<<"Wierszy, w ktorych pierwsze slowo jest anagramem drugiego, jest "<<anagram<<endl;
+		
+		slowaP.close();
+	}
+}
