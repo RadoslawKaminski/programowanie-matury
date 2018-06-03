d94bbb086c2010b59206c3f7d6af78737fb19aa4
Radosław <radziowojak@gmail.com>
2018-06-03 16:38:50 +0200

    wynik zadania 5 z 2018 stara podstawowa jest zapisywany do pliku

--- 2018 stara podstawowa/5.cpp
+++ 2018 stara podstawowa/5.cpp
@@ -14,7 +14,7 @@ int main()
 {
 	int liczby[1001];
 	ifstream liczbyP;
-	liczbyP.open("test.txt");
+	liczbyP.open("liczby.txt");
 	if(liczbyP.is_open())
 	{
 		int i=-1;
@@ -26,6 +26,7 @@ int main()
 		{
 			//5.1
 			cout<<"5.1:\n";
+			wynik5<<"5.1:\n";
 			int max;
 			int j=0;
 			do
@@ -37,9 +38,11 @@ int main()
 				if(liczby[k] > max && liczby[k]%2 == 0)
 					max = liczby[k];
 			cout<<"Najwieksza liczba parzysta to: "<<max<<endl;
+			wynik5<<"Najwieksza liczba parzysta to: "<<max<<endl;
 			
 			//5.2
-			cout<<"5.2:\n";
+			cout<<"\n5.2:\n";
+			wynik5<<"\n5.2:\n";
 			for(int k=0; k<i; k++)
 			{
 				string liczba = toString(liczby[k]);
@@ -49,6 +52,7 @@ int main()
 					{
 						if(l == liczba.length()/2-1)
 							cout<<liczba<<endl;
+							wynik5<<liczba<<endl;
 					}
 					else
 						break;
@@ -56,10 +60,12 @@ int main()
 			}
 			
 			//5.3
-			cout<<"5.3:\n";
+			cout<<"\n5.3:\n";
+			wynik5<<"\n5.3:\n";
 			int suma[1001]={};
 			int sumaS=0;
 			cout<<"Liczby, ktorych suma cyfr jest wieksza od 30:\n";
+			wynik5<<"Liczby, ktorych suma cyfr jest wieksza od 30:\n";
 			for(int k=0; k<i; k++)
 			{
 				int liczba = liczby[k];
@@ -69,12 +75,16 @@ int main()
 					liczba /= 10;
 				}
 				if(suma[k] > 30)
+				{
 					cout<<liczby[k]<<endl;
+					wynik5<<liczby[k]<<endl;
+				}
 				sumaS += suma[k];
 			}
 			cout<<"Wszystkie cyfry, ktore zostaly wykorzystane do zapisu wszystkich liczb w pliku to: ";
 			cout<<"1,2,3,4,5,6,7,8 oraz 9, ich suma wynosi 45\n";
 			cout<<"... Chyba, ze chodzi o sume wszystkich cyfr w pliku, wynosi ona "<<sumaS;
+			wynik5<<"Suma wszystkich cyfr w pliku wynosi "<<sumaS;
 			
 			wynik5.close();
 		}
