7d8697a2c55d6b26919bcd7fbfc004ea1b81c61d
Radosław <radziowojak@gmail.com>
2018-06-03 14:56:08 +0200

    dodano szablon zadania 5 z 2018 podstawowa w starej formule

---
+++ 2018 stara podstawowa/5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+//matura rozszerzona 2018 w starej formule
+int main()
+{
+	string liczby[99999];
+	ifstream liczbyP;
+	liczbyP.open("liczby.txt");
+	if(liczbyP.is_open())
+	{
+		int i=0;
+		while(liczbyP>>slowa[i++]);
+		
+		ofstream wynik5;
+		wynik5.open("wynik5.txt");
+		if(wynik6.is_open())
+		{
+			
+		}
+		liczbyP.close();
+	}
+}
