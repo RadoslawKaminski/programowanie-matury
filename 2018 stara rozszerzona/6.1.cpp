b315f77cb083cc7e58ea46ea663feff08852f1d8
Radosław <radziowojak@gmail.com>
2018-06-03 14:45:41 +0200

    wynik zadania 6 z 2018 rozszerzenia w starej formuje jest zapisywany do pliku

--- 2018 stara rozszerzona/6.1.cpp
+++ 2018 stara rozszerzona/6.1.cpp
@@ -13,47 +13,58 @@ int main()
 		int i=0;
 		while(slowaP>>slowa[i++]);
 		
-		//6.1
-		cout<<"6.1:\n";
-		int konczyA=0;
-		for(int j=0; j<i; j++)
-			if(slowa[j][ slowa[j].length()-1 ] == 'A')
-				konczyA++;
-		cout<<"Na 'A' konczy sie "<<konczyA<<" slow.\n";
-		
-		//6.2
-		cout<<"\n6.2:\n";
-		int zawiera=0;
-		for(int j=0; j<i-1; j+=2)
-			if(slowa[j+1].find(slowa[j]) != string::npos)
-			{
-				cout<<slowa[j]<<" zawiera sie w "<<slowa[j+1]<<endl;
-				zawiera++;
-			}
-		cout<<"Wierszy, w ktorych pierwsze slowo zawiera sie w drugim, jest "<<zawiera<<endl;
-		
-		//6.3
-		cout<<"\n6.3:\n";
-		int anagram=0;
-		for(int j=0; j<i-1; j+=2)
-			if(slowa[j].length() == slowa[j+1].length())
-			{
-				for(int k=0; k<slowa[j+1].length(); k++)
+		ofstream wynik6;
+		wynik6.open("wynik6.txt");
+		if(wynik6.is_open())
+		{
+			//6.1
+			cout<<"6.1:\n";
+			wynik6<<"6.1:\n";
+			int konczyA=0;
+			for(int j=0; j<i; j++)
+				if(slowa[j][ slowa[j].length()-1 ] == 'A')
+					konczyA++;
+			cout<<"Na 'A' konczy sie "<<konczyA<<" slow.\n";
+			wynik6<<"Na 'A' konczy sie "<<konczyA<<" slow.\n";
+			
+			//6.2
+			cout<<"\n6.2:\n";
+			wynik6<<"\n6.2:\n";
+			int zawiera=0;
+			for(int j=0; j<i-1; j+=2)
+				if(slowa[j+1].find(slowa[j]) != string::npos)
 				{
-					if(slowa[j+1].find(slowa[j][k]) != string::npos)
-					{
-						if(k == slowa[j+1].length()-1)
-						{
-							cout<<slowa[j]<<" jest anagramem "<<slowa[j+1]<<endl;
-							anagram++;
-						}
-					}
-					else
-						break;
+					cout<<slowa[j]<<" zawiera sie w "<<slowa[j+1]<<endl;
+					zawiera++;
 				}
-			}
-		cout<<"Wierszy, w ktorych pierwsze slowo jest anagramem drugiego, jest "<<anagram<<endl;
-		
+			cout<<"Wierszy, w ktorych pierwsze slowo zawiera sie w drugim, jest "<<zawiera<<endl;
+			wynik6<<"Wierszy, w ktorych pierwsze slowo zawiera sie w drugim, jest "<<zawiera<<endl;
+			
+			//6.3
+			cout<<"\n6.3:\n";
+			wynik6<<"\n6.3:\n";
+			int anagram=0;
+			for(int j=0; j<i-1; j+=2)
+				if(slowa[j].length() == slowa[j+1].length())
+				{
+					for(int k=0; k<slowa[j+1].length(); k++)
+					{
+						if(slowa[j+1].find(slowa[j][k]) != string::npos)
+						{
+							if(k == slowa[j+1].length()-1)
+							{
+								cout<<slowa[j]<<" jest anagramem "<<slowa[j+1]<<endl;
+								anagram++;
+							}
+						}
+						else
+							break;
+					}
+				}
+			cout<<"Wierszy, w ktorych pierwsze slowo jest anagramem drugiego, jest "<<anagram<<endl;
+			wynik6<<"Wierszy, w ktorych pierwsze slowo jest anagramem drugiego, jest "<<anagram<<endl;
+			
+		}
 		slowaP.close();
 	}
 }
