89b5fc456fa1a9ff1bf9031b5493ca1cdff0281a
Radosław <radziowojak@gmail.com>
2018-06-03 15:12:28 +0200

    zrobione zadanie 5.1 z 2018 podstawowej w starej formule

--- 2018 stara podstawowa/5.cpp
+++ 2018 stara podstawowa/5.cpp
@@ -11,13 +11,26 @@ int main()
 	if(liczbyP.is_open())
 	{
 		int i=-1;
-		while(liczbyP>>slowa[++i]);
+		while(liczbyP>>liczby[++i]);
 		
 		ofstream wynik5;
 		wynik5.open("wynik5.txt");
 		if(wynik5.is_open())
 		{
-			
+			//5.1
+			cout<<"5.1:\n";
+			int max;
+			int j=0;
+			do
+			{
+				max = liczby[j];
+				j++;
+			}while(liczby[j]%2 == 0 && j<i);
+			for(int k=0; k<i; k++)
+				if(liczby[k] > max && liczby[k]%2 == 0)
+					max = liczby[k];
+			cout<<"Najwieksza liczba parzysta to: "<<max;
+					
 			wynik5.close();
 		}
 		liczbyP.close();
